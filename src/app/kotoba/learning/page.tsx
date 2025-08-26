d183c8df37381c12c51f534b40780d1335ed21ae
Rahmad aryadi <rahmadaryadi17@gmail.com>
2025-08-26 13:10:01 +0000

    untuk memudahkan penambahan data yang sangat banyak, pisah data di sini,

--- src/app/kotoba/learning/page.tsx
+++ src/app/kotoba/learning/page.tsx
@@ -14,19 +14,7 @@ import { Progress } from "@/components/ui/progress";
 import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-
-const flashcards = [
-  { jp: "こんにちは", en: "Halo / Selamat siang" },
-  { jp: "ありがとう", en: "Terima kasih" },
-  { jp: "さようなら", en: "Selamat tinggal" },
-  { jp: "すみません", en: "Maaf / Permisi" },
-  { jp: "はい", en: "Ya" },
-  { jp: "いいえ", en: "Tidak" },
-  { jp: "日本", en: "Jepang" },
-  { jp: "私", en: "Saya" },
-  { jp: "猫", en: "Kucing" },
-  { jp: "犬", en: "Anjing" },
-];
+import { flashcards } from "@/lib/data/data";
 
 export default function LearningKotobaPage() {
   const [currentIndex, setCurrentIndex] = useState(0);
---
+++ src/lib/data/data.ts
@@ -0,0 +1,12 @@
+export const flashcards = [
+  { jp: "こんにちは", en: "Halo / Selamat siang" },
+  { jp: "ありがとう", en: "Terima kasih" },
+  { jp: "さようなら", en: "Selamat tinggal" },
+  { jp: "すみません", en: "Maaf / Permisi" },
+  { jp: "はい", en: "Ya" },
+  { jp: "いいえ", en: "Tidak" },
+  { jp: "日本", en: "Jepang" },
+  { jp: "私", en: "Saya" },
+  { jp: "猫", en: "Kucing" },
+  { jp: "犬", en: "Anjing" },
+];
