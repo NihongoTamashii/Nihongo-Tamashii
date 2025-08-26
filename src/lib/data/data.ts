7636dc1bf9dfd26642928e918b2ded024786e289
Rahmad aryadi <rahmadaryadi17@gmail.com>
2025-08-26 13:24:40 +0000

    ubah defaulr nya menjadi data dibawah ini
    {
        "front": "わたし",
        "fr

--- src/lib/data/data.ts
+++ src/lib/data/data.ts
@@ -1,12 +1,26 @@
 export const flashcards = [
-  { front: "こんにちは", frontSub: "Konnichiwa", back: "Halo / Selamat siang" },
-  { front: "ありがとう", frontSub: "Arigatou", back: "Terima kasih" },
-  { front: "さようなら", frontSub: "Sayounara", back: "Selamat tinggal" },
-  { front: "すみません", frontSub: "Sumimasen", back: "Maaf / Permisi" },
-  { front: "はい", frontSub: "Hai", back: "Ya" },
-  { front: "いいえ", frontSub: "Iie", back: "Tidak" },
-  { front: "日本", frontSub: "Nihon", back: "Jepang" },
-  { front: "わたし", frontSub: "私", back: "Saya" },
-  { front: "猫", frontSub: "Neko", back: "Kucing" },
-  { front: "犬", frontSub: "Inu", back: "Anjing" },
+  {
+    "front": "わたし",
+    "frontSub": "私",
+    "romaji": "watashi",
+    "back": "Saya"
+  },
+  {
+    "front": "あなた",
+    "frontSub": "",
+    "romaji": "anata",
+    "back": "Kamu"
+  },
+  {
+    "front": "あのひと",
+    "frontSub": "あの人",
+    "romaji": "ano hito",
+    "back": "Orang itu (biasa)"
+  },
+  {
+    "front": "あのかた",
+    "frontSub": "あの方",
+    "romaji": "ano kata",
+    "back": "Orang itu (sopan)"
+  }
 ];
