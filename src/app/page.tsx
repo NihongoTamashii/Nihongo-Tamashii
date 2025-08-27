29b83fb3cb777054fee2cea706be2107cd233204
Rahmad aryadi <rahmadaryadi17@gmail.com>
2025-08-27 09:34:48 +0000

    https://www.tiktok.com/@nihongo.tamashii (link tiktok)
    saweria.co/nihon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,7 +190,7 @@ export default function HomePage() {
                 <CardDescription>Bantu kami untuk terus berkembang dan menyediakan konten berkualitas.</CardDescription>
               </CardHeader>
               <CardContent>
-                <a href="https://saweria.co/Nihongotamashii" target="_blank" rel="noopener noreferrer">
+                <a href="https://saweria.co/nihongotamashii" target="_blank" rel="noopener noreferrer">
                   <Button className="w-full">
                     <svg className="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                       <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-12h2v2h-2zm0 4h2v6h-2z" />
@@ -206,3 +206,5 @@ export default function HomePage() {
     </SidebarProvider>
   );
 }
+
+    
