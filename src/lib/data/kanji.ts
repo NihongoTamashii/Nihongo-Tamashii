26e19cef7c07a002e16ff601e015d5d7432c528e
Rahmad aryadi <rahmadaryadi17@gmail.com>
2025-08-28 07:35:18 +0000

    data pertama

--- src/lib/data/kanji.ts
+++ src/lib/data/kanji.ts
@@ -1,4 +1,20 @@
 
 export const kanjiN5 = [
+  { front: '日', back: 'Onyomi: ニチ (nichi)\nKunyomi: ひ (hi)\nMeaning: Hari' },
+  { front: '人', back: 'Onyomi: ジン (jin)\nKunyomi: ひと (hito)\nMeaning: Orang' },
+  { front: '学', back: 'Onyomi: ガク (gaku)\nKunyomi: まなぶ (manabu)\nMeaning: Belajar' },
+  { front: '行', back: 'Onyomi: コウ (kou)\nKunyomi: いく (iku)\nMeaning: Pergi' },
+  { front: '見', back: 'Onyomi: ケン (ken)\nKunyomi: みる (miru)\nMeaning: Melihat' },
+  { front: '月', back: 'Onyomi: ゲツ (getsu)\nKunyomi: つき (tsuki)\nMeaning: Bulan' },
+  { front: '山', back: 'Onyomi: サン (san)\nKunyomi: やま (yama)\nMeaning: Gunung' },
+  { front: '川', back: 'Onyomi: セン (sen)\nKunyomi: かわ (kawa)\nMeaning: Sungai' },
+  { front: '水', back: 'Onyomi: スイ (sui)\nKunyomi: みず (mizu)\nMeaning: Air' },
+  { front: '大', back: 'Onyomi: ダイ (dai)\nKunyomi: おおきい (ookii)\nMeaning: Besar' },
+  { front: '本', back: 'Onyomi: ホン (hon)\nKunyomi: もと (moto)\nMeaning: Buku / Asal' },
+  { front: '小', back: 'Onyomi: ショウ (shou)\nKunyomi: ちいさい (chiisai)\nMeaning: Kecil' },
+  { front: '高', back: 'Onyomi: コウ (kou)\nKunyomi: たかい (takai)\nMeaning: Tinggi / Mahal' },
+  { front: '女', back: 'Onyomi: ジョ (jo)\nKunyomi: おんな (onna)\nMeaning: Perempuan' },
+  { front: '男', back: 'Onyomi: ダン (dan)\nKunyomi: おとこ (otoko)\nMeaning: Laki-laki' },
+  { front: '子', back: 'Onyomi: シ (shi)\nKunyomi: こ (ko)\nMeaning: Anak' },
 ];
 export const kanjiN4: any[] = [];
