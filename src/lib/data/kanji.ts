df45290f78203a4dd26557054fac9f7a10bc4fe3
Rahmad aryadi <rahmadaryadi17@gmail.com>
2025-08-28 07:18:52 +0000

    data baru untuk N5

--- src/lib/data/kanji.ts
+++ src/lib/data/kanji.ts
@@ -10,7 +10,7 @@ export const kanjiN5 = [
   },
   {
     front: "国",
-    back: "On'yomi: コク (koku)\nKun'yomi: - \nArti: negara",
+    back: "On'yomi: コク (koku)\nKun'yomi: くに (kuni)\nArti: negara",
   },
   {
     front: "人",
@@ -55,6 +55,70 @@ export const kanjiN5 = [
   {
     front: "時",
     back: "On'yomi: ジ (ji)\nKun'yomi: とき (toki)\nArti: waktu, jam",
+  },
+  {
+    front: "行",
+    back: "On'yomi: コウ (kō), ギョウ (gyō)\nKun'yomi: いく (iku), ゆく (yuku), おこなう (okonau)\nArti: pergi, perjalanan",
+  },
+  {
+    front: "見",
+    back: "On'yomi: ケン (ken)\nKun'yomi: みる (miru), みえる (mieru), みせる (miseru)\nArti: lihat, harapan, peluang, ide, opini",
+  },
+  {
+    front: "月",
+    back: "On'yomi: ゲツ (getsu), ガツ (gatsu)\nKun'yomi: つき (tsuki)\nArti: bulan (kalender), bulan (di langit)",
+  },
+  {
+    front: "後",
+    back: "On'yomi: ゴ (go), コウ (kō)\nKun'yomi: のち (nochi), うしろ (ushiro), あと (ato)\nArti: belakang, nanti",
+  },
+  {
+    front: "前",
+    back: "On'yomi: ゼン (zen)\nKun'yomi: まえ (mae)\nArti: depan, sebelum",
+  },
+  {
+    front: "生",
+    back: "On'yomi: セイ (sei), ショウ (shō)\nKun'yomi: いきる (ikiru), いかす (ikasu), うまれる (umareru), なま (nama)\nArti: kehidupan, asli, lahir",
+  },
+  {
+    front: "五",
+    back: "On'yomi: ゴ (go)\nKun'yomi: いつ (itsu)\nArti: lima",
+  },
+  {
+    front: "間",
+    back: "On'yomi: カン (kan), ケン (ken)\nKun'yomi: あいだ (aida), ま (ma)\nArti: interval, ruang",
+  },
+  {
+    front: "上",
+    back: "On'yomi: ジョウ (jō), ショウ (shō)\nKun'yomi: うえ (ue), あがる (agaru), のぼる (noboru)\nArti: atas, naik",
+  },
+  {
+    front: "東",
+    back: "On'yomi: トウ (tō)\nKun'yomi: ひがし (higashi)\nArti: timur",
+  },
+  {
+    front: "四",
+    back: "On'yomi: シ (shi)\nKun'yomi: よん (yon), よ (yo), よっつ (yottsu)\nArti: empat",
+  },
+  {
+    front: "今",
+    back: "On'yomi: コン (kon), キン (kin)\nKun'yomi: いま (ima)\nArti: sekarang",
+  },
+  {
+    front: "金",
+    back: "On'yomi: キン (kin), コン (kon)\nKun'yomi: かね (kane)\nArti: emas, uang",
+  },
+  {
+    front: "九",
+    back: "On'yomi: キュウ (kyū), ク (ku)\nKun'yomi: ここの (kokono), ここのつ (kokonotsu)\nArti: sembilan",
+  },
+  {
+    front: "入",
+    back: "On'yomi: ニュウ (nyū)\nKun'yomi: いる (iru), いれる (ireru), はいる (hairu)\nArti: masuk, memasukkan",
+  },
+  {
+    front: "学",
+    back: "On'yomi: ガク (gaku)\nKun'yomi: まなぶ (manabu)\nArti: belajar, ilmu",
   },
 ];
 export const kanjiN4: any[] = [];
